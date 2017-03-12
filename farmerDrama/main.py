f0d0f94bcb492890ed9bc4201089219c448c91e6
Alexander Su <alexanderytsu@gmail.com>
2017-03-12 10:42:35 +0000

    rough farmer idea

---
+++ farmerDrama/main.py
@@ -0,0 +1,19 @@
+# hold the number of fences in a counter
+# checking from the outside in, subtract everytime a fence does not need to be removed
+
+# setup
+fr = open('farmin.txt', 'r')
+n_plots = int(fr.readline())
+plots = fr.read().split()
+fr.close()
+plots = [int(i) for i in plots]
+fences = n_plots -1
+
+for x in range(n_plots):
+    a = x
+    b = n_plots - (x+1)
+    if plots[a] == plots[b]:
+        print(a,b)
+        fences -= 1 
+
+print(fences)
