a1feded97ed25173d5b2b06a6626df1c42cc41eb
Alexander Su <alexanderytsu@gmail.com>
2017-05-15 08:12:12 +0000

    safe attempt

---
+++ safeCracking/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    FILE *fin;
+    fin = fopen("safein.txt", "r");
+    int code_length;
+    fscanf(fin, "%d", &code_length);
+    int sub_length;
+    fscanf(fin, "%d", &sub_length);
+
+    int code[code_length];
+    int sub[sub_length];
+
+    for(int i = 0, i < code_length; i ++){
+        fscanf(fin, "%d", &code[i]);       
+    }
+
+    for(int i = 0; i < sub_length; i ++){
+        fscanf(fin, "%d", &sub[i]);
+    }
+
+    int shift = 0;
+
+    for(int shift = 0; shift < code_length - sub_length; i ++){
+        for(int i = 0; i < sub_length; i ++){
+            
+            
+
+        }
+    }
+
+    return 0;
+}
