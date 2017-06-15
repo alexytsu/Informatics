48a2077826138a50a3c87893f47c6daf27d2d255
Alexander Su <alexanderytsu@gmail.com>
2017-06-15 12:15:15 +1000

    solved safecracking 90%

--- safeCracking/main.c
+++ safeCracking/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(void){
 
     FILE *fin;
     fin = fopen("safein.txt", "r");
+
     int code_length;
     fscanf(fin, "%d", &code_length);
     int sub_length;
@@ -12,7 +14,7 @@ int main(int argc, char *argv[]){
     int code[code_length];
     int sub[sub_length];
 
-    for(int i = 0, i < code_length; i ++){
+    for(int i = 0; i < code_length; i ++){
         fscanf(fin, "%d", &code[i]);       
     }
 
@@ -20,7 +22,32 @@ int main(int argc, char *argv[]){
         fscanf(fin, "%d", &sub[i]);
     }
 
-    
 
-    return 0;
+    int offset = code_length - sub_length; 
+    int solved = 0;
+    int difference = 0;
+
+    while(!solved){
+
+        solved = 1;
+        difference = code[offset] - sub[0];
+        for(int i = 0; i < sub_length; i ++){
+            if(code[offset+i] - sub[i] != difference){
+                solved = 0;
+                offset --;
+                break;
+            }
+        }
+    }
+    
+    for(int i = 0; i < code_length; i ++){
+        code[i] -= difference;   
+    }
+     
+    FILE *fout = fopen("safeout.txt", "w");
+    for(int i = 0; i < code_length; i ++){
+        fprintf(fout, "%d\n", code[i]);
+    }
+    fclose(fout);
+    fclose(fin);
 }
