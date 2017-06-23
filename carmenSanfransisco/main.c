5a570837282bcbd41b747f750923dbf559ee6375
Alexander Su <alexanderytsu@gmail.com>
2017-06-23 23:45:32 +1000

    TODO: allow multiple of same connection per city

--- carmenSanfransisco/main.c
+++ carmenSanfransisco/main.c
@@ -2,14 +2,10 @@
 #include <stdlib.h>
 
 typedef struct city{
-    struct city *t1_in;
-    struct city *t1_out;
-    struct city *t2_in;
-    struct city *t2_out;
-    struct city *t3_in;
-    struct city *t3_out;
-    struct city *t4_in;
-    struct city *t4_out;
+    struct city *t1;
+    struct city *t2;
+    struct city *t3;
+    struct city *t4;
 }City;
 
 
@@ -23,11 +19,14 @@ int main(void){
 
     for(int i = 0; i < n_cities; i ++){
         city_array[i] = malloc(sizeof(City));
+        city_array[i]->t1=NULL;
+        city_array[i]->t2=NULL;
+        city_array[i]->t3=NULL;
+        city_array[i]->t4=NULL;
     }
 
     for(int i = 0; i < lines; i ++){
-
-        int city1; 
+        int city1;
         fscanf(fin, "%d", &city1);
         city1--;
         int city2;
@@ -35,5 +34,42 @@ int main(void){
         city2--;
         int transport;
         fscanf(fin, "%d", &transport);
+
+        if(transport == 1){
+            city_array[city1]->t1 = &city_array[city2];
+            city_array[city2]->t1 = &city_array[city1];
+        }else if(transport == 2){
+            city_array[city1]->t2 = &city_array[city2];
+            city_array[city2]->t2 = &city_array[city1];
+        }else if(transport == 3){
+            city_array[city1]->t3 = &city_array[city2];
+            city_array[city2]->t3 = &city_array[city1];
+        }
+        else if(transport == 4){
+            city_array[city1]->t4 = &city_array[city2];
+            city_array[city2]->t4 = &city_array[city1];
+        }else{
+            fprintf(stderr, "Invalid transport on line %d. Exiting...\n", i);
+            exit(1);
+        }
     }
+
+    int n = 0;
+    fscanf(fin, "%d", &n);
+    int sighting[n];
+    for(int i = 0; i < n; i ++){
+        fscanf(fin, "%d", &sighting[i]);
+    }
+
+    fclose(fin);
+
+    for(int i = 0; i < n_cities; i ++){
+        printf("City %d @pointer %p\n", i, &city_array[i]);
+        printf("\t Transport 1: %p\n", city_array[i]->t1);
+        printf("\t Transport 2: %p\n", city_array[i]->t2);
+        printf("\t Transport 3: %p\n", city_array[i]->t3);
+        printf("\t Transport 4: %p\n", city_array[i]->t4);
+    }
+
+    return 0;
 }
